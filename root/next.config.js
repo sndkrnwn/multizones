e0b6cd5c2bdb7a98fa1f30ffe8115ad0718cfb74
sandy.kurniawan <sandy.kurniawan@dataon.com>
2022-11-02 15:54:46 +0700

    setup multi zones

---
+++ root/next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const { CLIENT_URL } = process.env;
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `/:path*`,
+      },
+      {
+        source: "/client",
+        destination: `${CLIENT_URL}/client`,
+      },
+      {
+        source: "/client/:path*",
+        destination: `${CLIENT_URL}/client/:path*`,
+      },
+    ];
+  },
+}
+
+module.exports = nextConfig
