ad11ab7ca7e77e0c6a015623f08e7f67234c74d1
sandy.kurniawan <sandy.kurniawan@dataon.com>
2022-11-04 09:46:42 +0700

    enhancement structure client

--- root/next.config.js
+++ root/next.config.js
@@ -3,7 +3,7 @@ const { CLIENT_URL } = process.env;
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath="/root",
+  // basePath="/root",
   // assetPrefix: "client/",
   // async rewrites() {
   //   return [
